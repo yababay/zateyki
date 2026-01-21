6af8a160b2a7cc74c3bb78d23e623acfddc0628b
Михаил Беляков <bigbelk@live.ru>
2026-01-21 07:49:14 +0300

    first

---
+++ src/lib/components/Svg.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let svg: string;
+
+	const getSvg = () => {
+		const iof = svg.indexOf('<svg');
+		if (iof > -1) return svg.slice(iof);
+		return svg;
+	};
+
+	export let caption = '';
+</script>
+
+<figure class="text-center">
+	{@html getSvg()}
+	{#if caption}
+		<figcaption class="d-flex justify-content-center w-100">
+			<p class="p-3 text-start w-75">{caption}</p>
+		</figcaption>
+	{/if}
+</figure>
