f563040bbcddfa533a316ab4cb5a21359ad3f5fb
Михаил Беляков <bigbelk@live.ru>
2026-01-27 22:30:30 +0300

    from-article

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
-    	export let title: string = '', description: string = ''
+    	export let title: string = '', description: string = '', canonical: string = '', image: string = '' 
 </script>
 
 <svelte:head>
+    {#if canonical}
+        <link rel="canonical" href={canonical}>
+    {/if}
+    {#if image}
+        <meta property="image" content={image} />
+        <meta property="twitter:image" content={image} />
+        <meta property="og:image:width" content="1600" />
+        <meta property="og:image:height" content="840" />
+        <meta property="og:image" content={image} />
+    {/if}
     {#if description}
         <meta name="description" content={description}>
         <meta name="og:description" content={description}>
--- src/lib/components/Helper.svelte
+++ src/lib/components/Helper.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
     import Head from '$lib/components/Head.svelte';
-    import banner from '$lib/assets/barny-banner.png'
-    export let title: string, description: string
+    import _banner from '$lib/assets/barny-banner.png'
+    export let title: string, description: string, canonical: string = '', image: string = '', banner: string = ''
 </script>
 
-<Head {title} {description} />
+<Head {title} {description} {canonical} {image} />
 
 <slot>
-    <img src={banner} alt="banner" class="d-none">
+    <img src={banner || _banner} alt="banner" class="d-none">
 </slot>
+
+{#if image}
+    <img src={image} alt={title} >
+{/if}
+
+{#if canonical}
+    <p>Исходный материал <a href={canonical} title={title} target="_blank">здесь</a>.</p>
+{/if}
