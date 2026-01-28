6dbd8c78522226e9b7715f867b95379c4bdd91f5
Михаил Беляков <bigbelk@live.ru>
2026-01-28 16:14:30 +0300

    branch

---
+++ scripts/git-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. scripts/functions.sh
+
+set_envs
+
+git add .
+git commit -am ok 
+git checkout main
+git checkout "$CURRENT_BRANCH" -- "src/routes/\(longread\)/$CURRENT_BRANCH"
+git add .
+git commit -am "$CURRENT_BRANCH"
+git push origin main 
+git branch -D  "$CURRENT_BRANCH"
