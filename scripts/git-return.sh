71e843b3837c94621de2eb71b1ce6a7d5f32f197
Михаил Беляков <bigbelk@live.ru>
2026-01-28 17:16:29 +0300

    return

--- scripts/git-return.sh
+++ scripts/git-return.sh
@@ -7,7 +7,7 @@ set_envs
 git add .
 git commit -am ok 
 git checkout main
-git checkout "$CURRENT_BRANCH" -- "src/routes/\(longread\)/$CURRENT_BRANCH"
+git checkout "$CURRENT_BRANCH" -- "src/routes/(longread)/$CURRENT_BRANCH"
 git add .
 git commit -am "$CURRENT_BRANCH"
 git push origin main 
