76fdbb11680eaf73eb099d410060f86db3abdf4f
Михаил Беляков <bigbelk@live.ru>
2026-01-28 09:24:22 +0300

    3-articles

--- src/routes/(longread)/gastarbeiters-from-russian-empire/+page.ts
+++ src/routes/(longread)/gastarbeiters-from-russian-empire/+page.ts
@@ -1,8 +1,7 @@
 import { extractMdxMeta } from '$lib'
 import page from './+page.md?raw'
 import image from './image.webp'
-import banner from './banner.webp'
 
 export const load = () => {
-    return { ...extractMdxMeta(page), image, banner }
+    return { ...extractMdxMeta(page), image }
 }
