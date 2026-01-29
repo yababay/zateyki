90edcffced7193512b75847a77485ce8bf6698a8
Михаил Беляков <bigbelk@live.ru>
2026-01-29 04:32:27 +0300

    rm

--- scripts/optimize-images.sh
+++ scripts/optimize-images.sh
@@ -3,3 +3,4 @@
 DIR=`find 'src/routes/(longread)' -type d | tail -n 1` 
 
 npx image-transmutation --run --sourceFolder "$DIR" --targetFolder "$DIR" --inputFormats 'jpg' --inputFormats 'jpeg' --inputFormats 'png' --outputFormats 'webp'
+rm "$DIR/*.png" "$DIR/*.jpg" "$DIR/*.jpeg"
