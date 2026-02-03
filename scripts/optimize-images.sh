699aea59eebaaac8f2f49f1e9cf9ec8691b9c9ae
Михаил Беляков <bigbelk@live.ru>
2026-02-04 00:58:33 +0300

    sed

--- scripts/optimize-images.sh
+++ scripts/optimize-images.sh
@@ -5,3 +5,5 @@ DIR=`find 'src/routes/(longread)' -type d | tail -n 1`
 npx image-transmutation --run --sourceFolder "$DIR" --targetFolder "$DIR" --inputFormats 'jpg' --inputFormats 'jpeg' --inputFormats 'png' --outputFormats 'webp'
 
 find "$DIR" -name *.png -exec rm {} \;
+
+sed 's/\/\/\ //;s/\/\*//;s/\*\///' -i "$DIR/+page.ts"
