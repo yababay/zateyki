cd4a432dea4d64a4d5097b9b6913cd62d8c27315
Михаил Беляков <bigbelk@live.ru>
2026-02-14 00:41:32 +0300

    ch

---
+++ scripts/checkout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p 'Введите имя пути: ' SLUG 
+
+DIR="src/routes/(longread)/$SLUG"
+PAGE="$DIR/+page.md"
+CODE="$DIR/+page.ts"
+
+git checkout article
+git checkout -b "$SLUG"
+git checkout main -- "$DIR"
+
+git "src/routes/(longread)/zyxel-the-cat/+page.md" >> $PAGE
+cat "src/routes/(longread)/zyxel-the-cat/+page.ts" > $CODE
+git rm -r "src/routes/(longread)/zyxel-the-cat"
+
+/usr/bin/gnome-text-editor $PAGE &>/dev/null &
