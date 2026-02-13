9276a1c5f1cac3fa199fcdcc3cd0568f7c69c264
Михаил Беляков <bigbelk@live.ru>
2026-02-13 13:37:02 +0300

    sleep

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -8,12 +8,10 @@ PAGE=src/routes/\(longread\)/$CURRENT_BRANCH/+page.md
 
 git show main:src/routes/\(longread\)/$CURRENT_BRANCH/+page.md >> $PAGE
 
-sleep 1
-
 if [ -n "`git show main:src/routes/\(longread\)/$CURRENT_BRANCH/*.webp`" ]; then 
     git checkout main -- src/routes/\(longread\)/$CURRENT_BRANCH/*.webp
 fi
 
-sleep 1
+sleep 3
 
 /usr/bin/gnome-text-editor $PAGE &>/dev/null &
