960a34f41c4497cab0d6fc72149840ccf250f3ea
Михаил Беляков <bigbelk@live.ru>
2026-02-17 03:53:14 +0300

    pl

---
+++ src/lib/components/Prolog.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '$lib/styles/prolog.css'
+    export let div
+</script>
+
+<article>
+    {@html div}
+</article>
+
+<style>
+    article {
+        max-width: 80ch;
+    }
+</style>
