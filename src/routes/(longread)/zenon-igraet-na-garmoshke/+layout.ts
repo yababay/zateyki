5284372a577a2d5a39eccad98566e545cb07a0b2
Михаил Беляков <bigbelk@live.ru>
2026-06-01 14:14:07 +0300

    zenon-igraet-na-garmoshke

---
+++ src/routes/(longread)/zenon-igraet-na-garmoshke/+layout.ts
@@ -0,0 +1,8 @@
+import { extractMdxMeta } from '$lib'
+import page   from './+page.md?raw'
+//import banner from './banner.webp'
+import image  from './image.webp'
+
+export const load = () => {
+    return { ...extractMdxMeta(page), image /*, banner*/  }
+}
