41b948cc1a539bd31f1a3c9b5f20b30f13724448
Михаил Беляков <bigbelk@live.ru>
2026-06-18 10:05:52 +0300

    epub

--- scripts/epub.sh
+++ scripts/epub.sh
@@ -6,4 +6,19 @@ set_envs
 
 mkdir -p "$STATIC_DIR"
 
-pandoc "$CURRENT_DIR/+page.md" -o "STATIC_DIR/book.epub"
+TITLE=$(egrep -m 1 "^# " "$CURRENT_DIR/+page.md" | sed 's/# //')
+
+echo "Generating EPUB with title: $TITLE"
+
+rm "$STATIC_DIR/*.epub"
+
+pandoc "$CURRENT_DIR/+page.md" --metadata title="$TITLE" \
+  --toc \
+  --toc-depth=2 \
+  --katex \
+  --output "$STATIC_DIR/`uuidgen`.epub"
+
+#  --css "$CURRENT_DIR/epub.css" \
+#  --epub-cover-image "$CURRENT_DIR/cover.jpg" \
+#  --epub-metadata "$CURRENT_DIR/metadata.xml" \
+#  --epub-stylesheet "$CURRENT_DIR/epub.css" \
