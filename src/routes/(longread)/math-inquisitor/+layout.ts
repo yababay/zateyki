556a50cd8088411d3464c0b2a92f34f2ba8772e6
Михаил Беляков <bigbelk@live.ru>
2026-06-19 09:55:27 +0300

    math-inquisitor

--- src/routes/(longread)/math-inquisitor/+layout.ts
+++ src/routes/(longread)/math-inquisitor/+layout.ts
@@ -3,6 +3,9 @@ import page   from './+page.md?raw'
 import banner from './banner.webp'
 import image  from './image.webp'
 
+//const banner = undefined
+//const image = undefined
+
 export const load = () => {
     return { ...extractMdxMeta(page), image, banner  }
 }
